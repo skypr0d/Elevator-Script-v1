1ad69de58119391cc7fa2960631eb6873aaca3f3
Skypr0d <67479263+skypr0d@users.noreply.github.com>
2023-12-04 22:11:01 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,18 +3,6 @@ game 'gta5'
 lua54 'yes'
 
 client_scripts {
-    ----> Rage UI
-    "RageUI/RMenu.lua",
-    "RageUI/menu/RageUI.lua",
-    "RageUI/menu/Menu.lua",
-    "RageUI/menu/MenuController.lua",
-    "RageUI/components/*.lua",
-    "RageUI/menu/elements/*.lua",
-    "RageUI/menu/items/*.lua",
-    "RageUI/menu/panels/*.lua",
-    "RageUI/menu/windows/*.lua",
-    "RageUI/menu/Function.lua",
-
     'config.lua',
     'client.lua',
 }
